189a3cae217c85ee5c4f4801d797a7f3dddcd365
Rafał <rafcikp2@gmail.com>
2021-04-10 10:10:34 +0200

    add simple api

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('welcome.html', message="Welcome",)
+def welcome():
+    return render_template('welcome.html', message="Welcome", )
 
 
 @app.route('/settings')
 def settings():
     return 'Settings'
 
+
 @app.route('/card/<int:index>')
-def card_view(index):
+def show_card(index):
     try:
         card = db[index]
-        return render_template('card.html', card=card)
+        return render_template('card.html', card=card, index=index, max_index=len(db) - 1)
     except IndexError:
         abort(404)
+
+
+@app.route('/cards')
+def show_cards():
+    return render_template('cards.html', cards=db)
+
+
+@app.route('/api/card/<int:index>')
+def api_show_card(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
+
+
+@app.route('/api/cards')
+def api_show_cards():
+    return jsonify(db)
 
 
 if __name__ == '__main__':
